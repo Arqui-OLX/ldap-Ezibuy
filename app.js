e2e27132b7a68244c36c6f16f1d6b4b25ca66c04
JGironB <jpgironb@unal.edu.co>
2019-11-20 16:55:29 -0500

    Corregido problema de desconexion de socket

--- app.js
+++ app.js
@@ -3,39 +3,36 @@ const app = express();
 const bodyParser = require('body-parser');
 const ldap = require('ldapjs');
 
-const ldapClientAdmin = ldap.createClient({
-    url: 'ldap://35.208.241.159:389',
-    reconnect: true
-});
-
-const ldapClient = ldap.createClient({
-    url: 'ldap://35.208.241.159:389',
-    recconect: true
-})
-
-ldapClientAdmin.bind('cn=admin,dc=ezibuy,dc=com,dc=co', 'password', function(err) {
-    if(!err)
-        console.log("Succesfully connected to LDAP server")
-    else
-        console.error("Error connecting to LDAP server")
-});
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
+const ldapConnectionOptions = {
+    url: 'ldap://35.208.241.159:389',
+    reconnect: true
+}
+
 app.post('/login', function (req, res) {
+
+    let ldapClient = ldap.createClient(ldapConnectionOptions);
+
     const dn = `cn=${req.body.email},ou=ezibuyteam,dc=ezibuy,dc=com,dc=co`;
     const password = req.body.password;
 
     ldapClient.bind(dn, password, function(err) {
-        if(!err)
+        if(!err) {
             res.status(200).send(true);
+            ldapClient.unbind();
+        }
         else
             res.status(200).send(false);
     });
 })
 
 app.post('/insert', function (req, res) {
+
+    let ldapClient = ldap.createClient(ldapConnectionOptions);
+
     const dn = `cn=${req.body.email},ou=ezibuyteam,dc=ezibuy,dc=com,dc=co`;
     const password = req.body.password;
 
@@ -45,18 +42,29 @@ app.post('/insert', function (req, res) {
         objectclass: 'inetOrgPerson',
         userPassword: password
     };
+
+    ldapClient.bind('cn=admin,dc=ezibuy,dc=com,dc=co', 'password', function(err) {
+        if(!err) {
+            ldapClient.add(dn, entry, function(err){
+                if(!err)
+                    res.status(200).send(true);
+                else 
+                    res.status(200).send(false); 
+                    
+                ldapClient.unbind();
+            })
+        }
+        else
+            res.status(200).send(false);
+    });
+
     
-    ldapClientAdmin.add(dn, entry, function(err){
-        if(!err){
-            res.status(200).send(true);
-        } else {
-            res.status(200).send(false); 
-            
-         }
-    })
 })
 
 app.post('/modify_password', function (req, res) {
+
+    let ldapClient = ldap.createClient(ldapConnectionOptions);
+
     const dn = `cn=${req.body.email},ou=ezibuyteam,dc=ezibuy,dc=com,dc=co`;
     const password = req.body.newPassword;
 
@@ -67,27 +75,57 @@ app.post('/modify_password', function (req, res) {
         }
     });
 
-    ldapClientAdmin.modify(dn, change, function(err){
-        if(!err)
-            res.status(200).send(true);
+
+    ldapClient.bind('cn=admin,dc=ezibuy,dc=com,dc=co', 'password', function(err) {
+        if(!err) {
+            ldapClient.modify(dn, change, function(err){
+                if(!err) 
+                    res.status(200).send(true);
+                     
+                else 
+                    res.status(200).send(false); 
+
+                ldapClient.unbind();
+            });
+        }
         else
             res.status(200).send(false);
     });
 
+
+
 })
 
 app.get('/exists', function(req,response) {
+
+    let ldapClient = ldap.createClient(ldapConnectionOptions);
+
     const dn = `cn=${req.body.email},ou=ezibuyteam,dc=ezibuy,dc=com,dc=co`;
 
-    ldapClientAdmin.search(dn, dn, function(err, res){
+
+    ldapClient.bind('cn=admin,dc=ezibuy,dc=com,dc=co', 'password', function(err) {
+        if(!err) {
+            ldapClient.search(dn, dn, function(err, res){
         
-          res.on('error', function(err) {
-            response.status(200).send(false);
+                res.on('error', function(err) {
+                    
+                  response.status(200).send(false);
+                  ldapClient.unbind();
+                });
+                res.on('end', function(result) {
+                  response.status(200).send(true);
+                  ldapClient.unbind();
+                });
+
+                
           });
-          res.on('end', function(result) {
-            response.status(200).send(true);
-          });
+        }
+        else {
+            
+            res.status(200).send(false); }
     });
+
+
 })
 
 app.use((req, res) => {
@@ -95,7 +133,6 @@ app.use((req, res) => {
         error: "invalid route"
     })
 });
-
 
 
 module.exports = app;
